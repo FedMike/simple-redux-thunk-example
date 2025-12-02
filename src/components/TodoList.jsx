4f66c7de24c6685842b369200e84a147d669a339
fedmike <molnek87@mail.ru>
2025-12-02 16:49:37 +0900

    added preloader and error status

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,13 @@
 import { useSelector } from "react-redux";
 
 const TodoList = () => {
-  const todos = useSelector((state) => state.todos);
+  const { list: todos, status, error } = useSelector((state) => state.todos);
 
-  return <div>Todos: {todos.length}</div>;
+  return (
+    <div>
+      Todos: {todos.length}. Status: {status} {error && <h4>{error}</h4>}
+    </div>
+  );
 };
 
 export { TodoList };
--- src/store/todos/todo-reducer.js
+++ src/store/todos/todo-reducer.js
@@ -1,9 +1,31 @@
-import { ADD_TODOS } from "./todo-actions";
+import { ADD_TODOS, SER_ERROR, SET_LOADING } from "./todo-actions";
 
-export const todoReducer = (state = [], action) => {
+const initialState = {
+    status: 'idle',
+    list: [],
+    error: null,
+};
+
+export const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TODOS:
-            return action.payload;
+            return {
+                ...state,
+                list: action.payload,
+                status: 'fullfilled',
+            };
+        case SET_LOADING:
+            return {
+                ...state,
+                status: 'loading',
+                error: null,
+            }
+        case SER_ERROR:
+            return {
+                ...state,
+                status: 'rejected',
+                error: action.payload,
+            }
         default:
             return state;
     }
